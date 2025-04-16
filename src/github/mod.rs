9dae52d0399b55747fd2b0efb3d3779b0b969e1f
Tom Plant <tom@tplant.com.au>
2025-04-16 14:01:57 +0000

    Vendoring, IDE settings, CI, custom winget-pkgs
    Signed-off-by: Tom Plant <tom@tplant.com.au>

--- src/github/mod.rs
+++ src/github/mod.rs
@@ -7,7 +7,7 @@ pub mod utils;
 use const_format::formatcp;
 pub use error::GitHubError;
 
-pub const MICROSOFT: &str = "microsoft";
-pub const WINGET_PKGS: &str = "winget-pkgs";
+pub const MICROSOFT: &str = "pl4nty";
+pub const WINGET_PKGS: &str = "winget-extras";
 pub const WINGET_PKGS_FULL_NAME: &str = formatcp!("{MICROSOFT}/{WINGET_PKGS}");
 pub const GITHUB_HOST: &str = "github.com";
