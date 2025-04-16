67f96f2538e46b39a533b9ee9ea9d46b11074c80
Tom Plant <tom@tplant.com.au>
2025-04-16 14:01:57 +0000

    Vendor inno, IDE settings, CI, custom winget-pkgs
    Signed-off-by: Tom Plant <tom@tplant.com.au>

--- src/github/mod.rs
+++ src/github/mod.rs
@@ -4,10 +4,9 @@ pub mod graphql;
 mod rest;
 pub mod utils;
 
-use const_format::formatcp;
 pub use error::GitHubError;
 
 pub const MICROSOFT: &str = "microsoft";
 pub const WINGET_PKGS: &str = "winget-pkgs";
-pub const WINGET_PKGS_FULL_NAME: &str = formatcp!("{MICROSOFT}/{WINGET_PKGS}");
+pub const WINGET_PKGS_FULL_NAME: &str = "pl4nty/winget-pkgs-selfhost";
 pub const GITHUB_HOST: &str = "github.com";
