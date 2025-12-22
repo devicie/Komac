46d8d3b12b39b80c7dd97ec6e2ce60405eff0b9e
Tom Plant <tom.plant@devicie.com>
2025-12-22 11:28:02 +0000

    Analyse Squirrel.Windows and Velopack
    Signed-off-by: Tom Plant <tom.plant@devicie.com>

--- src/analysis/installers/exe.rs
+++ src/analysis/installers/exe.rs
@@ -5,9 +5,9 @@ use inno::{Inno, error::InnoError};
 use winget_types::installer::{Architecture, Installer, InstallerType};
 use yara_x::mods::PE;
 
-use super::{super::Installers, Burn, Nsis};
+use super::{super::Installers, Burn, Nsis, Squirrel};
 use crate::{
-    analysis::installers::{burn::BurnError, nsis::NsisError},
+    analysis::installers::{burn::BurnError, nsis::NsisError, squirrel::SquirrelError},
     traits::FromMachine,
 };
 
@@ -19,6 +19,7 @@ pub enum Exe {
     Burn(Box<Burn>),
     Inno(Box<Inno>),
     Nsis(Nsis),
+    Squirrel(Squirrel),
     Generic(Box<Installer>),
 }
 
@@ -39,6 +40,12 @@ impl Exe {
         match Nsis::new(&mut reader, pe) {
             Ok(nsis) => return Ok(Self::Nsis(nsis)),
             Err(NsisError::NotNsisFile) => {}
+            Err(error) => return Err(error.into()),
+        }
+
+        match Squirrel::new(&mut reader, pe) {
+            Ok(squirrel) => return Ok(Self::Squirrel(squirrel)),
+            Err(SquirrelError::NotSquirrelFile) => {}
             Err(error) => return Err(error.into()),
         }
 
@@ -69,6 +76,7 @@ impl Installers for Exe {
             Self::Burn(burn) => burn.installers(),
             Self::Inno(inno) => inno.installers(),
             Self::Nsis(nsis) => nsis.installers(),
+            Self::Squirrel(squirrel) => squirrel.installers(),
             Self::Generic(installer) => vec![*installer.clone()],
         }
     }
