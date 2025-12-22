39a100301ab3ee0b70268fd0f6031384bee0206b
Tom Plant <tom.plant@devicie.com>
2025-12-22 11:28:02 +0000

    Analyse Squirrel.Windows and Velopack
    Signed-off-by: Tom Plant <tom.plant@devicie.com>

--- src/analysis/installers/mod.rs
+++ src/analysis/installers/mod.rs
@@ -4,6 +4,7 @@ pub mod inno;
 mod msi;
 pub mod msix_family;
 pub mod nsis;
+pub mod squirrel;
 pub mod utils;
 mod zip;
 
@@ -11,4 +12,5 @@ pub use burn::Burn;
 pub use exe::Exe;
 pub use msi::Msi;
 pub use nsis::Nsis;
+pub use squirrel::Squirrel;
 pub use zip::Zip;
