e3d48e2dafc9246255c8d89b83a8f916f8026dad
Tom Plant <tom.plant@devicie.com>
2026-01-21 02:37:19 +0000

    squash: feat/analyse-7zsfx

--- src/analysis/installers/mod.rs
+++ src/analysis/installers/mod.rs
@@ -6,6 +6,7 @@ mod installshield;
 mod msi;
 pub mod msix_family;
 pub mod nsis;
+mod sevenzip_sfx;
 pub mod utils;
 mod zip;
 
@@ -15,4 +16,5 @@ pub use exe::Exe;
 pub use installshield::InstallShield;
 pub use msi::Msi;
 pub use nsis::Nsis;
+pub use sevenzip_sfx::SevenZipSfx;
 pub use zip::Zip;
