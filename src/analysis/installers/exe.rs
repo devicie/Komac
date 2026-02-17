ce4af7b7268c66531a525614209d03b7578f5039
Tom Plant <tom.plant@devicie.com>
2026-02-17 08:26:47 +0000

    squash: feat/analyse-advanced-installer

--- src/analysis/installers/exe.rs
+++ src/analysis/installers/exe.rs
@@ -6,9 +6,9 @@ use tracing::debug;
 use winget_types::installer::{Architecture, Installer, InstallerSwitches, InstallerType};
 use yara_x::mods::PE;
 
-use super::{super::Installers, Burn, Nsis};
+use super::{super::Installers, AdvancedInstaller, Burn, Nsis};
 use crate::{
-    analysis::installers::{burn::BurnError, nsis::NsisError},
+    analysis::installers::{advanced::AdvancedInstallerError, burn::BurnError, nsis::NsisError},
     traits::FromMachine,
 };
 
@@ -17,6 +17,7 @@ const FILE_DESCRIPTION: &str = "FileDescription";
 const BASIC_INSTALLER_KEYWORDS: [&str; 4] = ["installer", "setup", "7zs.sfx", "7zsd.sfx"];
 
 pub enum Exe {
+    AdvancedInstaller(Box<AdvancedInstaller>),
     Burn(Box<Burn>),
     Inno(Box<Inno>),
     Nsis(Nsis),
@@ -25,6 +26,12 @@ pub enum Exe {
 
 impl Exe {
     pub fn new<R: Read + Seek>(mut reader: R, pe: &PE) -> Result<Self> {
+        match AdvancedInstaller::new(&mut reader, pe) {
+            Ok(advanced) => return Ok(Self::AdvancedInstaller(Box::new(advanced))),
+            Err(AdvancedInstallerError::NotAdvancedInstallerFile) => {}
+            Err(error) => return Err(error.into()),
+        }
+
         match Burn::new(&mut reader, pe) {
             Ok(burn) => return Ok(Self::Burn(Box::new(burn))),
             Err(BurnError::NotBurnFile) => {}
@@ -95,6 +102,7 @@ impl Exe {
 impl Installers for Exe {
     fn installers(&self) -> Vec<Installer> {
         match self {
+            Self::AdvancedInstaller(advanced) => advanced.installers(),
             Self::Burn(burn) => burn.installers(),
             Self::Inno(inno) => inno.installers(),
             Self::Nsis(nsis) => nsis.installers(),
