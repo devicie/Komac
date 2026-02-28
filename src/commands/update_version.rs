5a886497d6bc12c2d719e6c0e43acc1905cda056
Tom Plant <tom.plant@devicie.com>
2026-02-28 00:44:30 +0000

    Add installers during update
    Signed-off-by: Tom Plant <tom.plant@devicie.com>

--- src/commands/update_version.rs
+++ src/commands/update_version.rs
@@ -1,6 +1,7 @@
 use std::{
     collections::BTreeSet,
     collections::HashMap,
+    collections::HashSet,
     io::{Read, Seek},
     mem,
     num::{NonZeroU32, NonZeroUsize},
@@ -202,7 +203,7 @@ impl UpdateVersion {
             .collect::<Vec<_>>();
         manifests.default_locale.package_version = package_version.clone();
         let matched_installers = match_installers(previous_installers, &installer_results);
-        let installers = matched_installers
+        let mut installers = matched_installers
             .into_iter()
             .map(|(previous_installer, new_installer)| {
                 let analyser = &download_results[&new_installer.url];
@@ -240,6 +241,17 @@ impl UpdateVersion {
                 installer
             })
             .collect::<Vec<_>>();
+
+        let matched_urls = installers
+            .iter()
+            .map(|installer| installer.url.clone())
+            .collect::<HashSet<_>>();
+
+        installers.extend(
+            installer_results
+                .into_iter()
+                .filter(|installer| !matched_urls.contains(&installer.url)),
+        );
 
         manifests.installer.package_version = package_version.clone();
         manifests.installer.minimum_os_version = manifests
