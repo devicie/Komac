ca0e8a4bc8853d46768f3aea902082867c1c3c4d
Tom Plant <tom.plant@devicie.com>
2026-03-30 06:57:57 +0000

    feat: auto-update disable vanity, support common channels, cache strategies to file
    Signed-off-by: Tom Plant <tom.plant@devicie.com>

--- src/github/rest/tree/mod.rs
+++ src/github/rest/tree/mod.rs
@@ -5,6 +5,7 @@ mod r#type;
 use std::collections::BTreeSet;
 
 use color_eyre::eyre::eyre;
+use futures_util::{StreamExt, stream};
 pub use git_file_mode::GitFileMode;
 use itertools::Itertools;
 pub use object::TreeObject;
@@ -28,12 +29,98 @@ pub struct GitTree {
 }
 
 impl GitHub {
-    pub async fn get_package_identifiers(
+    pub async fn get_package_identifiers_for_letter(
         &self,
+        letter: Option<char>,
     ) -> Result<BTreeSet<PackageIdentifier>, GitHubError> {
-        let endpoint = format!(
-            "{REST_API_URL}/repos/{MICROSOFT}/{WINGET_PKGS}/git/trees/HEAD:manifests?recursive=1"
-        );
+        let mut first_level_paths = if let Some(letter) = letter {
+            vec![format!("manifests/{}", letter.to_ascii_lowercase())]
+        } else {
+            let root = self
+                .get_git_tree(MICROSOFT, WINGET_PKGS, "manifests", false)
+                .await?;
+
+            root.tree
+                .iter()
+                .filter(|entry| entry.is_tree())
+                .map(|entry| format!("manifests/{}", entry.path))
+                .collect::<Vec<_>>()
+        };
+
+        first_level_paths.sort_unstable();
+
+        let package_identifiers =
+            stream::iter(first_level_paths.into_iter().map(|path| async move {
+                self.collect_package_identifiers_from_path(MICROSOFT, WINGET_PKGS, &path)
+                    .await
+            }))
+            .buffer_unordered(6)
+            .collect::<Vec<_>>()
+            .await
+            .into_iter()
+            .try_fold(BTreeSet::new(), |mut acc, result| {
+                let identifiers = result?;
+                acc.extend(identifiers);
+                Ok::<_, GitHubError>(acc)
+            })?;
+
+        if package_identifiers.is_empty() {
+            Err(GitHubError::GraphQL(eyre!(
+                "failed to enumerate package identifiers from winget-pkgs"
+            )))
+        } else {
+            Ok(package_identifiers)
+        }
+    }
+
+    async fn collect_package_identifiers_from_path(
+        &self,
+        owner: &str,
+        repo: &str,
+        root_path: &str,
+    ) -> Result<BTreeSet<PackageIdentifier>, GitHubError> {
+        let mut package_identifiers = BTreeSet::new();
+        let mut stack = vec![root_path.to_owned()];
+
+        while let Some(path) = stack.pop() {
+            let recursive_tree = self.get_git_tree(owner, repo, &path, true).await?;
+
+            if !recursive_tree.truncated {
+                package_identifiers.extend(parse_package_identifiers(&recursive_tree.tree));
+                continue;
+            }
+
+            let non_recursive_tree = self.get_git_tree(owner, repo, &path, false).await?;
+            let subdirectories = non_recursive_tree
+                .tree
+                .iter()
+                .filter(|entry| entry.is_tree())
+                .map(|entry| format!("{path}/{}", entry.path))
+                .collect::<Vec<_>>();
+
+            if subdirectories.is_empty() {
+                package_identifiers.extend(parse_package_identifiers(&recursive_tree.tree));
+                continue;
+            }
+
+            stack.extend(subdirectories);
+        }
+
+        Ok(package_identifiers)
+    }
+
+    async fn get_git_tree(
+        &self,
+        owner: &str,
+        repo: &str,
+        path: &str,
+        recursive: bool,
+    ) -> Result<GitTree, GitHubError> {
+        let endpoint = if recursive {
+            format!("{REST_API_URL}/repos/{owner}/{repo}/git/trees/HEAD:{path}?recursive=1")
+        } else {
+            format!("{REST_API_URL}/repos/{owner}/{repo}/git/trees/HEAD:{path}")
+        };
 
         let response = self
             .0
@@ -44,23 +131,7 @@ impl GitHub {
             .await?
             .error_for_status()?;
 
-        let GitTree { tree, .. } = response.json::<GitTree>().await?;
-
-        let package_identifiers = tree
-            .iter()
-            .filter(|entry| entry.is_blob())
-            .filter_map(|entry| entry.path.rsplit('/').next())
-            .filter_map(|file_name| file_name.strip_suffix(".installer.yaml"))
-            .filter_map(|identifier| identifier.parse::<PackageIdentifier>().ok())
-            .collect::<BTreeSet<_>>();
-
-        if package_identifiers.is_empty() {
-            Err(GitHubError::GraphQL(eyre!(
-                "failed to enumerate package identifiers from winget-pkgs"
-            )))
-        } else {
-            Ok(package_identifiers)
-        }
+        Ok(response.json::<GitTree>().await?)
     }
 
     pub async fn get_versions(
@@ -128,4 +199,13 @@ impl GitHub {
             Ok(versions)
         }
     }
+}
+
+fn parse_package_identifiers(tree: &[TreeObject]) -> BTreeSet<PackageIdentifier> {
+    tree.iter()
+        .filter(|entry| entry.is_blob())
+        .filter_map(|entry| entry.path.rsplit('/').next())
+        .filter_map(|file_name| file_name.strip_suffix(".installer.yaml"))
+        .filter_map(|identifier| identifier.parse::<PackageIdentifier>().ok())
+        .collect::<BTreeSet<_>>()
 }
