7dd1f78357a3dbd2e9d880e698733f5c8a3cc4ee
Tom Plant <tom.plant@devicie.com>
2026-03-30 06:57:57 +0000

    feat: auto-update disable vanity, support common channels, cache strategies to file
    Signed-off-by: Tom Plant <tom.plant@devicie.com>

--- src/github/rest/releases.rs
+++ src/github/rest/releases.rs
@@ -12,6 +12,8 @@ pub struct LatestRelease {
     pub tag_name: String,
     pub html_url: DecodedUrl,
     pub assets: Vec<ReleaseAsset>,
+    pub prerelease: bool,
+    pub draft: bool,
 }
 
 #[derive(Debug, Deserialize)]
@@ -20,12 +22,32 @@ pub struct ReleaseAsset {
 }
 
 impl GitHub {
-    pub async fn get_latest_release(
+    pub async fn get_releases(
         &self,
         owner: &str,
         repo: &str,
+    ) -> Result<Vec<LatestRelease>, GitHubError> {
+        let endpoint = format!("{REST_API_URL}/repos/{owner}/{repo}/releases?per_page=30");
+
+        let response = self
+            .0
+            .get(endpoint)
+            .header(ACCEPT, GITHUB_JSON_MIME)
+            .header(X_GITHUB_API_VERSION, REST_API_VERSION)
+            .send()
+            .await?
+            .error_for_status()?;
+
+        Ok(response.json::<Vec<LatestRelease>>().await?)
+    }
+
+    pub async fn get_release_by_tag(
+        &self,
+        owner: &str,
+        repo: &str,
+        tag: &str,
     ) -> Result<LatestRelease, GitHubError> {
-        let endpoint = format!("{REST_API_URL}/repos/{owner}/{repo}/releases/latest");
+        let endpoint = format!("{REST_API_URL}/repos/{owner}/{repo}/releases/tags/{tag}");
 
         let response = self
             .0
